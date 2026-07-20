e02ae67142cb6d90a0864a6f66fb8cae6ccce280
Dexter Cullados <jddexter2006@gmail.com>
2026-07-20 19:08:36 +0800

    Add the backbone of the system

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
